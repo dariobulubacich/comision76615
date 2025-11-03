b1f1bebb3e6d1449ba82487a1594250a27037c55
dario <dariobulubacich>
2025-11-03 09:27:18 -0300

    correccion index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./src/app");
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`✅ Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`✅ Example app listening on port http://localhost:${PORT}`);
 });
